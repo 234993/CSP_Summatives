0befbb1e21215533c18aa0224470011032f37f87
234993 <234993@shakopeeschools.org>
2025-10-27 15:38:52 -0500

    Update board_game_Janessa.py

--- 1.2.5 - Shall We Play a Game/board_game_Janessa.py
+++ 1.2.5 - Shall We Play a Game/board_game_Janessa.py
@@ -1,40 +1,42 @@
 #todo players turtles 
 import turtle as trtl
-
-player_one = trtl.Turtle()
+import random as rand #lets me roll dice 
+player_one = trtl.Turtle() #create players as turtles so they can move
 player_two = trtl.Turtle()
 
-player_one_avatar = ("blue_ship.gif")
+player_one_avatar = ("blue_ship.gif") 
 player_two_avatar = ("red_ship.gif")
 button_clicker = ("button.gif")
 #todo window start screen 
 wn = trtl.Screen()
-wn.bgpic("pirates_board_game_backg.gif")
-wn.addshape(player_one_avatar)
-wn.addshape(player_two_avatar)
-wn.addshape(button_clicker)
+wn.bgpic("pirates_board_game_backg.gif") # background to base movments on 
+wn.addshape(player_one_avatar) #add images to the program  
+wn.addshape(player_two_avatar) 
+wn.addshape(button_clicker) #add a cover that says button 
 
 #todo players movement 
-wn.update()
-player_one.shape("blue_ship.gif")
+wn.update() #makes sure to appers on the screen 
+player_one.shape("blue_ship.gif")#add images over my turtles
 player_two.shape("red_ship.gif")
 player_one.penup()
 player_two.penup()
-player_one.goto(-600,345)
+player_one.goto(-600,345)# opposite sides of the board at the start squares 
 player_two.goto(540,-335)
 player_one.pendown()
 player_two.pendown()
 
 # dice button- use a turtle to do this 
-button = trtl .Turtle()
+button = trtl .Turtle() #create to buttuon 
+button.hideturtle()
 button.shape("button.gif")
 button.penup()
-button.hideturtle()
 button.goto(650,350)
 button.showturtle()
 
-# dice number appering on the screen(in the chest)
 
+
+
+# dice number appering on the screen(in the chest)
 
 
 # How to reconize when the game is won 
