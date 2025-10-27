35662c26a96e1cea79a92a1004ba249b655509ab
234993 <234993@shakopeeschools.org>
2025-10-27 11:02:29 -0500

    Create board_game_Janessa.py
    Initial commit with comments on milestone

---
+++ 1.2.5 - Shall We Play a Game/board_game_Janessa.py
@@ -0,0 +1,27 @@
+#todo window start screen 
+
+
+
+#todo players turtles 
+
+
+#todo players movement 
+
+
+
+# dice button 
+
+
+
+
+# dice number appering on the screen(in the chest)
+
+
+
+# How to reconize when the game is won 
+
+
+
+
+
+# option to end or restart the game 
