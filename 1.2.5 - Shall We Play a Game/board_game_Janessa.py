50f368b45fa0f64278bc837600340512ad8f3849
234993 <234993@shakopeeschools.org>
2025-10-27 12:44:48 -0500

    fix the players appering

--- 1.2.5 - Shall We Play a Game/board_game_Janessa.py
+++ 1.2.5 - Shall We Play a Game/board_game_Janessa.py
@@ -1,16 +1,25 @@
 #todo players turtles 
 import turtle as trtl
-import turtle as player_one
-import turtle as player_two 
-player_one = ("blue_ship.gif")
-player_two = ("red_ship")
+
+player_one = trtl.Turtle()
+player_two = trtl.Turtle()
+
+player_one_avatar = ("blue_ship.gif")
+player_two_avatar = ("red_ship.gif")
 
 #todo window start screen 
 wn = trtl.Screen()
 wn.bgpic("pirates_board_game_backg.gif")
+wn.addshape(player_one_avatar)
+wn.addshape(player_two_avatar)
+
 #todo players movement 
-
-
+player_one.penup()
+player_two.penup()
+player_one.goto(-100,100)
+player_two.goto(-100,100)
+player_one.pendown()
+player_two.pendown()
 
 # dice button 
 
