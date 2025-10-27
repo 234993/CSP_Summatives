e872dd7fb8096ff21fac568a367ef9605acfe592
234993 <234993@shakopeeschools.org>
2025-10-27 13:33:59 -0500

    start working on button

--- 1.2.5 - Shall We Play a Game/board_game_Janessa.py
+++ 1.2.5 - Shall We Play a Game/board_game_Janessa.py
@@ -6,13 +6,13 @@ player_two = trtl.Turtle()
 
 player_one_avatar = ("blue_ship.gif")
 player_two_avatar = ("red_ship.gif")
-
+button_clicker = ("button.gif")
 #todo window start screen 
 wn = trtl.Screen()
 wn.bgpic("pirates_board_game_backg.gif")
 wn.addshape(player_one_avatar)
 wn.addshape(player_two_avatar)
-
+wn.addshape(button_clicker)
 
 #todo players movement 
 wn.update()
@@ -25,9 +25,13 @@ player_two.goto(540,-335)
 player_one.pendown()
 player_two.pendown()
 
-# dice button- need to look up the method for this 
-
-
+# dice button- use a turtle to do this 
+button = trtl .Turtle()
+button.shape("button.gif")
+button.penup()
+button.hideturtle()
+button.goto(650,350)
+button.showturtle()
 
 # dice number appering on the screen(in the chest)
 
