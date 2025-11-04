61a0a3f37fb9854ab86908d07ac02e9b88ed2111
234993 <234993@shakopeeschools.org>
2025-11-04 13:31:44 -0600

    Editied-Final code

--- 1.2.5 - Shall We Play a Game/board_game_Janessa.py
+++ 1.2.5 - Shall We Play a Game/board_game_Janessa.py
@@ -1,17 +1,14 @@
 #todo players turtles 
 import turtle as trtl
 import random as rand #lets me roll dice
-global time 
 import time  
-global player_one 
-
-
 
 
 #Turtles created-------------------------------------------------------------------
 player_one = trtl.Turtle() #create players as turtles so they can move
 player_two = trtl.Turtle()
 wn = trtl.Screen()
+wn.tracer(0)
 button = trtl.Turtle() #create to push button 
 button_2 = trtl.Turtle()
 rolled_number = trtl.Turtle() 
@@ -20,88 +17,122 @@ player_2_score = trtl.Turtle()
 player_one_win = trtl.Turtle()
 player_two_win = trtl.Turtle()
 warning_message = trtl.Turtle()
-#add images over the turtles-------------------------------------------------------------------------------------
 
-player_one_avatar = ("blue_ship.gif") 
-player_two_avatar = ("red_ship.gif")
-button_clicker_1 = ("button.gif")
-button_clicker_2 = ("button.gif")
+#make the screen bigger 
+wn.setup(width=1500, height=1000) 
 
-#todo window start screen 
-wn.bgpic("pirates_board_game_backg.gif") # background to base movments on 
+#make the game setup together with a def 
+def setup():
+    global time, player_one, player_two, button, button_2, rolled_number
+    global player_1_score, player_2_score, player_one_win, player_two_win
+    global warning_message, position_1_p1, position_1_p2, player_show_score, player2_show_score
 
-#add images as shapes 
-wn.addshape(player_one_avatar) #add images to the program  
-wn.addshape(player_two_avatar) 
-wn.addshape(button_clicker_1) #add a cover that says button 
-wn.addshape(button_clicker_2) #add a cover that says button 
 
-#players 
-player_one.shape("blue_ship.gif")#add images over my turtles
-player_two.shape("red_ship.gif")
 
-#buttons 
-button.shape("button.gif")
-button_2.shape("button.gif")
+    #add images over the turtles-------------------------------------------------------------------------------------
 
-#todo players start movement-----------------------------------------------------------------------------------------------
- 
-wn.update() #makes sure to appers on the screen 
-#player one
-player_one.penup()
-player_one.goto(-600,345)# opposite sides of the board at the start squares 
-player_one.pendown()
-player_one.setheading(0)
-#player two 
-player_two.penup()
-player_two.goto(530,-335)
-player_two.pendown()
-player_two.setheading(180)
+    player_one_avatar = ("blue_ship.gif") 
+    player_two_avatar = ("red_ship.gif")
+    button_clicker_1 = ("button.gif")
+    button_clicker_2 = ("button.gif")
 
-#the number on the screen moved to the chest 
-rolled_number.penup()
-rolled_number.hideturtle()
-rolled_number.goto(100,100)
+    #todo window start screen 
+    wn.bgpic("pirates_board_game_backg.gif") # background to base movments on 
 
-#button one 
-button.hideturtle()
-button.penup()
-button.goto(650,350)
-button.showturtle()
-#button two 
-button_2.hideturtle()
-button_2.penup()
-button_2.goto(650,-350)
-button_2.showturtle()
+    #add images as shapes 
+    wn.addshape(player_one_avatar) #add images to the program  
+    wn.addshape(player_two_avatar) 
+    wn.addshape(button_clicker_1) #add a cover that says button 
+    wn.addshape(button_clicker_2) #add a cover that says button 
 
-#player ones score 
-player_1_score.hideturtle()
-player_1_score.penup()
-player_1_score.goto(-670,360) 
+    #players 
+    player_one.shape("blue_ship.gif")#add images over my turtles
+    player_two.shape("red_ship.gif")
 
-# player two score
-player_2_score.hideturtle()
-player_2_score.penup()
-player_2_score.goto(530,-405) 
+    #buttons 
+    button.shape("button.gif")
+    button_2.shape("button.gif")
 
-# player one dislplay if win 
-player_one_win.hideturtle()
-player_one_win.penup()
-player_one_win.goto(0,0) 
+    #todo players start movement-----------------------------------------------------------------------------------------------
+    
+    wn.update() #makes sure to appers on the screen 
+    #player one
+    player_one.penup()
+    player_one.goto(-600,345)# opposite sides of the board at the start squares 
+    player_one.pendown()
+    player_one.setheading(0)
+    #player two 
+    player_two.penup()
+    player_two.goto(530,-335)
+    player_two.pendown()
+    player_two.setheading(180)
 
-# player two display if win 
-player_two_win.hideturtle()
-player_two_win.penup()
-player_two_win.goto(0,0)
+    #the number on the screen moved to the chest 
+    rolled_number.penup()
+    rolled_number.hideturtle()
+    rolled_number.goto(100,100)
 
-# Warning message in the begining 
-warning_message.hideturtle()
-warning_message.penup()
-warning_message.goto(-500,0)
+    #button one 
+    button.hideturtle()
+    button.penup()
+    button.goto(650,350)
+    button.showturtle()
+    #button two 
+    button_2.hideturtle()
+    button_2.penup()
+    button_2.goto(650,-350)
+    button_2.showturtle()
 
-#Todo try target points----------------------------------------------------------------------------------------------------------------------
+    #player ones score 
+    player_1_score.hideturtle()
+    player_1_score.penup()
+    player_1_score.goto(-670,360) 
+
+    # player two score
+    player_2_score.hideturtle()
+    player_2_score.penup()
+    player_2_score.goto(530,-405) 
+
+    # player one dislplay if win 
+    player_one_win.hideturtle()
+    player_one_win.penup()
+    player_one_win.goto(0,0) 
+
+    # player two display if win 
+    player_two_win.hideturtle()
+    player_two_win.penup()
+    player_two_win.goto(0,0)
+
+    # Warning message in the begining 
+    warning_message.hideturtle()
+    warning_message.penup()
+    warning_message.goto(-500,0)
+
+#end of setup
+setup()
 position_1_p1 = 0
 position_1_p2 = 0
+# player ones and twos scores
+player_show_score = 0 
+player2_show_score =0
+def clear():
+    global position_1_p1, position_1_p2, player2_show_score, player_show_score
+    position_1_p1 = 0
+    position_1_p2 = 0
+    # player ones and twos scores
+    player_show_score = 0 
+    player2_show_score =0
+    player_one.clear()
+    player_two.clear()
+    rolled_number.clear()
+    player_1_score.clear()
+    player_2_score.clear()
+    player_one_win.clear()
+    player_two_win.clear()
+    warning_message.clear()
+    player_one.showturtle()
+    player_two.showturtle()
+
 # effects of landing on certain spaces for player one 
 p1_consequence_1 = [8,19,27]
 p1_consequence_3 = [4,23]
@@ -112,22 +143,20 @@ p2_consequence_1 = [3,11,24]
 p2_consequence_3 =[7,20]
 p2_boost = [13,29]
 
-# player ones and twos scores
-player_show_score = 0 
-player2_show_score =0
 
-#warning message before start-----Reduce the chance of the code bugging out---------------------------------------------------------------------------- 
-warning_message.write("Warning Message: Let the player complete the movment before pushing button again!", font=("Arial", 30, "normal"))
-time.sleep(5)
-warning_message.clear()
+#update screen
+wn.update()
+wn.tracer(True)
 
 
+#dummy function so the user cannot double roll
 
+def dummy(x,y):
+    print("What for movment to complete")
 # dice number appering on the screen(in the chest) + #todo have the players move and take turns-------------------------------------------------
-def roll_dice_1(x,y): #used x and y becuase i needed some sort of 2 arguments in the def 
-    global position_1_p1
-    global player_1_score
-    global player_show_score
+def roll_dice_1(x,y): #same as roll_dice_2
+    global button, position_1_p1, player_1_score, player_show_score
+    button.onclick(dummy)
     button_pushed = rand.randint(1,5) #had to make out of 7 becuase thats the shortest part of the board
     rolled_number.goto(-50,30) 
     rolled_number.write(button_pushed, font=("Arial", 30, "normal")) #found in the leaderboard code
@@ -142,14 +171,8 @@ def roll_dice_1(x,y): #used x and y becuase i needed some sort of 2 arguments in
                 player_one_win.write("player one wins!!!!!", font=("Arial", 40, "normal"))
                 player_one.goto(-450,200)
                 player_one.stamp()
-                player_one.hideturtle()
                 player_two.hideturtle()
-                restrat_game = trtl.textinput("Restart Game:", "Would you like to restart the game?") # use this to ask about restarting the game 
-                if restrat_game == "yes":
-                    roll_dice_1
-                    roll_dice_2
-                else:
-                    wn.bye()
+                look_winner(player_one)
             if player_show_score == 2:
                 player_one.goto(-350,150)
                 player_one.stamp()
@@ -177,20 +200,31 @@ def roll_dice_1(x,y): #used x and y becuase i needed some sort of 2 arguments in
     if position_1_p1 in p1_boost:
         player_one.forward(110)
         position_1_p1 +=1 
+    button.onclick(roll_dice_1)    
   
 button.onclick(roll_dice_1)
-# player twos section------------------------------------------------------------------------------------------------------------
 
-def roll_dice_2(x,y):
-    global position_1_p2  
-    global player_2_score
-    global player2_show_score
-    global restrat_game
-    button_2 = rand.randint(1,5) #had to make out of 7 becuase thats the shortest part of the board
+def look_winner(player):
+    player.hideturtle()
+    restrat_game = trtl.textinput("Restart Game:", "Would you like to restart the game?") # use this to ask about restarting the game 
+    if restrat_game == "yes":
+        clear()
+        setup()
+        player_one.penup()
+        player_two.penup()
+        #working on it 
+    else:
+        wn.bye()
+# player twos section------------------------------------------------------------------------------------------------------------
+dummy
+def roll_dice_2(x,y):# can i use x and y because it kinda dose noting but act as a place holder
+    global button_2, player2_show_score, position_1_p2
+    button_2.onclick(dummy) 
+    button_2_pushed = rand.randint(1,5) #had to make out of 7 becuase thats the shortest part of the board
     rolled_number.goto(-50,30) 
-    rolled_number.write(button_2, font=("Arial", 30, "normal")) #found in the leaderboard code
+    rolled_number.write(button_2_pushed, font=("Arial", 30, "normal")) #found in the leaderboard code
     player_two.penup()
-    for x in range(button_2):
+    for x in range(button_2_pushed):
         if position_1_p2  == 32:
             position_1_p2 = 0 #resets back to start value
             player_two.setheading(180)
@@ -199,16 +233,10 @@ def roll_dice_2(x,y):
             time.sleep(0.5)
             if player2_show_score == 3: #how to reconize when someone won 
                 player_two_win.write("player two wins!!!!!", font=("Arial", 40, "normal"))
-                player_two.goto(100,-50)
+                player_two.goto(150,-50)
                 player_two.stamp()
-                player_two.hideturtle()
                 player_one.hideturtle()
-                restrat_game = trtl.textinput("Restart Game:", "Would you like to restart the game?") # use this to ask about restarting the game 
-                if restrat_game == "yes":
-                    roll_dice_1
-                    roll_dice_2
-                else:
-                    wn.bye()
+                look_winner(player_two)
             if player2_show_score == 2:
                 player_two.goto(250,-130)
                 player_two.stamp()
@@ -236,13 +264,9 @@ def roll_dice_2(x,y):
     if position_1_p2 in p2_boost:# only need one of these 
         player_two.forward(110)
         position_1_p2 +=1
-  
+    button_2.onclick(roll_dice_2)
 
 button_2.onclick(roll_dice_2)
-
-
-
-
 
 
 # option to end or restart the game 
