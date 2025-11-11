50b894c072defd964fca20a9b5652c43a88a73a9
234993 <234993@shakopeeschools.org>
2025-11-11 10:19:57 -0600

    Plan

---
+++ 1.2.5 - Shall We Play a Game/Zoo_card_partner.py
@@ -0,0 +1,32 @@
+#Ask player for name 
+
+
+#display message with captured name 
+
+
+
+# change background 
+
+
+
+#set condtions for layout 
+
+
+
+#number list 
+
+
+
+#Leaderboard 
+
+
+
+
+#Timer/timer limt for each problem  
+
+
+
+#Set condtions for math problems 
+
+
+#Restart/try again 
