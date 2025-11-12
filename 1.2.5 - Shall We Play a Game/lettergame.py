78b530022a31083318d2ecc5b4abbdcc754e2884
234993 <234993@shakopeeschools.org>
2025-11-12 10:38:49 -0600

    Got the set up
    working on the numbers now

--- 1.2.5 - Shall We Play a Game/lettergame.py
+++ 1.2.5 - Shall We Play a Game/lettergame.py
@@ -5,7 +5,11 @@ wn.hideturtle()
 wn = trtl.Screen()
 square  = trtl.Turtle()
 plus = trtl.Turtle()
+times = trtl.Turtle()
+minus = trtl.Turtle()
+divide = trtl.Turtle()
 plus.penup()
+
 #Ask player for name 
 player = trtl.textinput("Name:", "Hello what is your name?")
 player = trtl.textinput("Message",  player +", Are you ready to escape to the zoo?")
@@ -20,8 +24,21 @@ else:
 
 
 # change background 
+wn.addshape("plus.gif")
+plus.penup()
+plus.shape("plus.gif")
 
+wn.addshape("times.gif")
+times.penup()
+times.shape("times.gif")
 
+wn.addshape("minus.gif")
+minus.penup()
+minus.shape("minus.gif")
+
+wn.addshape("divide.gif")
+divide.penup()
+divide.shape("divide.gif")
 
 #set condtions for layout 
 square.penup()
@@ -33,22 +50,37 @@ def place():
     square.penup()
     square.stamp()
 
-def plussymbol():
-    wn.addshape("plus.gif")
-    plus.penup()
-    plus.shape("plus.gif")
-    plus.stamp()
+
+divide.hideturtle()
+plus.hideturtle()
+minus.hideturtle()
+times.hideturtle()
+def symbol_set_up():
+  plus.goto(100, -225)
+  plus.stamp()  
+  minus.goto(-100, -225)
+  minus.stamp()
+  times.goto(0, -25)
+  times.stamp()
+  divide.goto(-100, 175)
+  divide.stamp()
+  minus.goto(100, 175)
+  minus.stamp()
+  plus.goto(0, -425)
+  plus.stamp()
+  plus.goto(0, 375)
+  plus.stamp()
 
 square.goto(0, -425)
 place()
-plus.goto(0, -425)
-plussymbol()
 
 square.goto(100, -225)
 place()
 
+
 square.goto(-100, -225)
 place()
+
 
 square.goto(0, -25)
 place()
@@ -61,8 +93,13 @@ place()
 
 square.goto(0, 375)
 place()
-plus.goto(0, 375)
-plussymbol()
+
+symbol_set_up()
+divide.showturtle()
+plus.showturtle()
+minus.showturtle()
+times.showturtle()
+
 #number list 
 number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
